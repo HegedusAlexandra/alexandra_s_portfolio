4b20800b4afc6c4760a90aca688e313dd21a6ad8
HegedusAlexandra <hexapastry@gmail.com>
2023-02-02 12:24:43 +0100

    Landing ready

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="menu">
-      <div id="menuContainer">h</div>
-      <button id="menuButton">MENU</button>
+      <div id="menuContainer">
+        <h1>Portfolio</h1>
+        <h2>Alexandra Hegedüs</h2>
+        <div className="bend">
+          <div className="greenSquare"></div>
+          <div className="blueSquare"></div>
+          <div className="redSquare">
+            <button id="menuButton">MENU</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
