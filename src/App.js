0818ce63ea89919b42ddc04b9e8822d19f21420b
HegedusAlexandra <hexapastry@gmail.com>
2023-02-28 17:29:14 +0100

    started project part

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { React, useEffect, useState } from "react";
+import { React } from "react";
 import Menu from "./components/Menu";
 
 function App() {
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -7,42 +7,41 @@ import "./components.css";
 import OMAS from "./pictures/OMAS.gif";
 import CV from "./pictures/CV.gif";
 import GAMES from "./pictures/GAMES.gif";
-import { useHorizontalScroll } from "./HorizontalScroll";
+/* import { useHorizontalScroll } from "./HorizontalScroll"; */
 
 export default function Projects() {
-  const scrollRef = useHorizontalScroll();
+  /* const scrollRef = useHorizontalScroll(); */
   return (
-    <div className="project" ref={scrollRef}>
+    <div className="project" /* ref={scrollRef} */>
       <Menu title={"Projects"} />
       <Scroll />
       <div className="imgContainer" style={{ whiteSpace: "nowrap" }}>
         <div className="imgBox">
           <img src={GAMES} alt="oops :( somthing went wrong" />
-          <a href="https://hegedusalexandra.github.io/small_projects/">
-            Check it out!
-          </a>
-          <h2 className="description">Small games with JavaScript</h2>
+          <div className="desc-container">
+            <a href="https://hegedusalexandra.github.io/small_projects/">
+              Check it out!
+            </a>
+            <h2 className="description">Small games with JavaScript</h2>
+          </div>
         </div>
         <div className="imgBox">
           <img src={CV} alt="oops :( somthing went wrong" />
-          <a href="https://hegedusalexandra.github.io/CurriculumVITAE/">
-            Check it out!
-          </a>
-          <h2 className="description">My patissier CV site with React</h2>
+          <div className="desc-container">
+            <a href="https://hegedusalexandra.github.io/CurriculumVITAE/">
+              Check it out!
+            </a>
+            <h2 className="description">My patissier CV site with React</h2>
+          </div>
         </div>
         <div className="imgBox">
           <img src={OMAS} alt="oops :( somthing went wrong" />
-          <a href="https://hegedusalexandra.github.io/OMASKLEIDERSCHRANK/">
-            Check it out!
-          </a>
-          <h2 className="description">React website for clothing shop</h2>
-        </div>
-        <div className="imgBox">
-          <img src={CV} alt="oops :( somthing went wrong" />
-          <a href="https://hegedusalexandra.github.io/CurriculumVITAE/">
-            Check it out!
-          </a>
-          <h2 className="description">My patissier CV site with React</h2>
+          <div className="desc-container">
+            <a href="https://hegedusalexandra.github.io/OMASKLEIDERSCHRANK/">
+              Check it out!
+            </a>
+            <h2 className="description">React website for clothing shop</h2>
+          </div>
         </div>
       </div>
     </div>
